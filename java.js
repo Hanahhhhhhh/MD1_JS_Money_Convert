697690f7bba3ef85452fbfe0c314d14eb79d22b2
hanahhhhhhhh <hanguyen18052004@gmail.com>
2022-03-28 15:22:54 +0700

    update code

--- java.js
+++ java.js
@@ -1,13 +1,12 @@
 let Convert1 = document.getElementById('button_convert');
-Convert1.addEventListener('click',click);
-function click(){
+Convert1.addEventListener('click', click);
+
+function click() {
     let amount = document.getElementById('input').value;
-    parseInt(amount)
     let fCurrency = document.getElementById('select1').value;
-    parseInt(fCurrency)
     let tCurrency = document.getElementById('select2').value;
-    parseInt(tCurrency)
-    let result;
-    result = amount * tCurrency / fCurrency;
+    let result = amount * fCurrency / tCurrency;
+    parseInt(result)
+
     document.getElementById('result').innerHTML = 'Result: ' + result;
 }
